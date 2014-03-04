67b6398ce455ea73fc182dfde9e295db1d843900
Eric Schulz <eschulz2@gmail.com>
2014-03-04 17:39:33 -0600

    server working, socket.io installed

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-// here
+var util = require("util"),
+    io = require("socket.io");
+
+var socket,
+    players;
+
+function init(){
+	players = [];
+	socket = io.listen(8000);
+
+	socket.configure(function(){
+		socket.set("transports", ["websocket"]);
+		socket.set("log level", 2);
+
+	});
+};
+
+init();
