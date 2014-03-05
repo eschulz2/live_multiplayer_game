20053e3a37da171b1421e6c023d8b960909a0b87
Eric Schulz <eschulz2@gmail.com>
2014-03-05 14:31:41 -0600

    game.js in progress

--- game.js
+++ game.js
@@ -13,6 +13,30 @@ function init(){
 		socket.set("log level", 2);
 
 	});
+	setEventHandlers();
+};
+
+var setEventHandlers = function() {
+    socket.sockets.on("connection", onSocketConnection);
+};
+
+function onSocketConnection(client) {
+    util.log("New player has connected: "+client.id);
+    client.on("disconnect", onClientDisconnect);
+    client.on("new player", onNewPlayer);
+    client.on("move player", onMovePlayer);
+};
+
+function onClientDisconnect() {
+    util.log("Player has disconnected: "+this.id);
+};
+
+function onNewPlayer(data) {
+
+};
+
+function onMovePlayer(data) {
+
 };
 
 init();
